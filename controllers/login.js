3c2163caccf0c63a56abf4a60094a6a0b489b5dc
adin234 <adinbautista@gmail.com>
2014-12-08 16:38:40 +0800

    hard code the session

--- controllers/login.js
+++ controllers/login.js
@@ -76,7 +76,7 @@ exports.authenticate = function (req, res, next) {
 exports.get_user = function(req, res, next) {
 	var data = {},
 		start = function() {
-			var cookie = req.cookies.anytv_xf_session || '';
+			var cookie = req.cookies.anytv_xf_session || req.query.session || '';
 
 			mysql.open(config.mysql)
 				.query(
